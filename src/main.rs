11227d8da03323e96b7e0c05b8bd125af24a9551
Soma Szabo <szabosonka@gmail.com>
2024-06-20 18:07:41 +0200

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use raylib::prelude::*;
+
+fn main(){
+    let (mut rl, thread) = raylib::init()
+    .size(1200,900)
+    .title("Hello World!")
+    .build();
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+         
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
+}
